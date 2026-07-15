bbca637fc54182031c733b367dacb7315a6602a7
mamoalves221-creator <mamoalves221@gmail.com>
2026-07-15 15:24:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,5 @@ def proxy(path):
     return Response(resp.content, resp.status_code, dict(resp.headers))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
