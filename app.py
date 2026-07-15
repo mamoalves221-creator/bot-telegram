f49d20baea59eb0c1003fc266bb1ad9613e7c997
mamoalves221-creator <mamoalves221@gmail.com>
2026-07-15 14:45:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,27 @@
 import socket, threading, os
 
-def handle(client):
+def pipe(src, dst):
     try:
-        # Lê o pedido inicial
-        request = client.recv(4096).decode()
-        if not request.startswith("CONNECT"):
-            client.close()
-            return
-        
-        # Extrai o host e a porta
-        host = request.split(" ")[1].split(":")[0]
-        port = int(request.split(" ")[1].split(":")[1])
-        
-        # Conecta ao destino
-        target = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        target.connect((host, port))
-        client.send(b"HTTP/1.1 200 Connection Established\r\n\r\n")
-        
-        def pipe(a, b):
-            try:
-                while True:
-                    d = a.recv(4096)
-                    if not d: break
-                    b.send(d)
-            except: pass
-        
-        threading.Thread(target=pipe, args=(client, target)).start()
-        pipe(target, client)
+        while True:
+            data = src.recv(4096)
+            if not data: break
+            dst.send(data)
     except: pass
     finally:
-        client.close()
+        src.close()
+        dst.close()
+
+def handle(client):
+    target = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Conecta diretamente ao destino (Facebook)
+    target.connect(("www.facebook.com", 443))
+    threading.Thread(target=pipe, args=(client, target)).start()
+    pipe(target, client)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("0.0.0.0", int(os.environ.get("PORT", 8080))))
 server.listen(5)
+
 while True:
-    c, _ = server.accept()
-    threading.Thread(target=handle, args=(c,)).start()
+    client, _ = server.accept()
+    threading.Thread(target=handle, args=(client,)).start()
