4b4204656eaa81f4ea8aae38ae5b723711c3d00a
mamoalves221-creator <mamoalves221@gmail.com>
2026-07-15 13:00:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
-import socket, threading, os
+import socket
+import threading
+import socks  # Biblioteca PySocks
+import os
 
-def handle(client):
-    target = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    target.connect(("www.facebook.com", 443))
-    def pipe(a, b):
-        try:
-            while True:
-                d = a.recv(4096)
-                if not d: break
-                b.send(d)
-        except: pass
-    threading.Thread(target=pipe, args=(client, target)).start()
-    pipe(target, client)
-    client.close()
-    target.close()
+def handle_client(client_socket):
+    try:
+        # Lê o cabeçalho inicial do SOCKS5
+        client_socket.recv(4096)
+        client_socket.send(b"\x05\x00") # Resposta de autenticação SOCKS5
+        
+        # Conecta ao destino
+        target = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        target.connect(("www.facebook.com", 443))
+        
+        def pipe(src, dst):
+            try:
+                while True:
+                    data = src.recv(4096)
+                    if not data: break
+                    dst.send(data)
+            except: pass
+            
+        threading.Thread(target=pipe, args=(client_socket, target)).start()
+        pipe(target, client_socket)
+    except: pass
+    finally:
+        client_socket.close()
 
-def run():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(("0.0.0.0", int(os.environ.get("PORT", 8080))))
-    server.listen(5)
-    while True:
-        c, _ = server.accept()
-        threading.Thread(target=handle, args=(c,)).start()
-
-if __name__ == "__main__":
-    run()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("0.0.0.0", int(os.environ.get("PORT", 8080))))
+server.listen(5)
+while True:
+    client, _ = server.accept()
+    threading.Thread(target=handle_client, args=(client,)).start()
