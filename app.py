e374c1fd05adcd5a0e0b5283f5181be277a41ad8
mamoalves221-creator <mamoalves221@gmail.com>
2026-07-15 13:09:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,39 @@
-import socket
-import threading
-import socks  # Biblioteca PySocks
-import os
+import socket, threading, os
 
-def handle_client(client_socket):
+def handle(client):
     try:
-        # Lê o cabeçalho inicial do SOCKS5
-        client_socket.recv(4096)
-        client_socket.send(b"\x05\x00") # Resposta de autenticação SOCKS5
+        # Lê o pedido inicial
+        request = client.recv(4096).decode()
+        if not request.startswith("CONNECT"):
+            client.close()
+            return
+        
+        # Extrai o host e a porta
+        host = request.split(" ")[1].split(":")[0]
+        port = int(request.split(" ")[1].split(":")[1])
         
         # Conecta ao destino
         target = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        target.connect(("www.facebook.com", 443))
+        target.connect((host, port))
+        client.send(b"HTTP/1.1 200 Connection Established\r\n\r\n")
         
-        def pipe(src, dst):
+        def pipe(a, b):
             try:
                 while True:
-                    data = src.recv(4096)
-                    if not data: break
-                    dst.send(data)
+                    d = a.recv(4096)
+                    if not d: break
+                    b.send(d)
             except: pass
-            
-        threading.Thread(target=pipe, args=(client_socket, target)).start()
-        pipe(target, client_socket)
+        
+        threading.Thread(target=pipe, args=(client, target)).start()
+        pipe(target, client)
     except: pass
     finally:
-        client_socket.close()
+        client.close()
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("0.0.0.0", int(os.environ.get("PORT", 8080))))
 server.listen(5)
 while True:
-    client, _ = server.accept()
-    threading.Thread(target=handle_client, args=(client,)).start()
+    c, _ = server.accept()
+    threading.Thread(target=handle, args=(c,)).start()
