181931b292bf574d0bc17640692c029fa37987c6
mamoalves221-creator <mamoalves221@gmail.com>
2026-07-15 14:52:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,13 @@ def pipe(src, dst):
         dst.close()
 
 def handle(client):
-    target = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Conecta diretamente ao destino (Facebook)
-    target.connect(("www.facebook.com", 443))
-    threading.Thread(target=pipe, args=(client, target)).start()
-    pipe(target, client)
+    try:
+        # AQUI ESTÁ O SEGREDO: Ignoramos qualquer handshake e conectamos direto ao destino
+        target = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        target.connect(("www.facebook.com", 443))
+        threading.Thread(target=pipe, args=(client, target)).start()
+        pipe(target, client)
+    except: client.close()
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("0.0.0.0", int(os.environ.get("PORT", 8080))))
